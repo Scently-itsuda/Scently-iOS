94445586593f7e67b1a3bde25aacf0894bbebe5c
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-04-28 16:56:19 +0900

    Initial Commit

---
+++ Scently/Scently/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scently
+//
+//  Created by 임재현 on 4/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hi")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
