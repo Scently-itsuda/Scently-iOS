e36485be6eb3a518fb1d42d4c95f0d705253eebf
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-06-18 11:02:08 +0900

    [feat] APIService Request 함수 구현

--- Scently/Scently/API/Core/APIService.swift
+++ Scently/Scently/API/Core/APIService.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+
+final class APIService {
+    static let shared = APIService()
+    private init() {}
+    
+    private let baseURL: String = ""
+    
+    func request<T:Decodable>(
+        path: String,
+        method: HTTPMethod,
+        headers: [String: String]? = nil,
+        body: Data? = nil,
+        responseType: T.Type
+        
+    ) async throws -> T {
+        guard let url = URL(string: baseURL + path) else {
+            throw NetworkError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        if let headers {
+            for (key, value) in headers {
+                request.setValue(value, forHTTPHeaderField: key)
+            }
+        }
+        request.httpBody = body
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.responseError
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw configureHTTPError(errorCode: httpResponse.statusCode)
+        }
+        
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch let decodingError as DecodingError {
+            print("DecodingError: \(decodingError)")
+            throw NetworkError.responseDecodingError
+        } catch {
+            print("일반 에러: \(error)")
+            throw NetworkError.responseDecodingError
+        }
+    }
+    
+    private func configureHTTPError(errorCode: Int) -> Error {
+        return NetworkError(rawValue: errorCode)
+        ?? NetworkError.unknownError
+    }
+}
