94bece20d55750ae0f1dc108b5e9f78c7c5a68ea
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-07-13 20:46:07 +0900

    [feat] NetworkService, NetworkServiceProtocol 객체 생성

---
+++ Scently/Scently/API/Core/NetworkService.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkService.swift
+//  Scently
+//
+//  Created by 임재현 on 7/13/25.
+//
+
+import Moya
+import Combine
+import Foundation
+
+class NetworkService<Target: TargetType>: NetworkServiceProtocol {
+    
+    private let provider: MoyaProvider<Target>
+    
+    init(provider: MoyaProvider<Target> = MoyaProvider<Target>()) {
+        self.provider = provider
+    }
+    
+    func request<T: Codable>(_ target: Target, responseType: T.Type) -> AnyPublisher<T, Error> {
+        
+        return provider.requestPublisher(target)
+            .map(\.data)
+            .decode(type: T.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+}
