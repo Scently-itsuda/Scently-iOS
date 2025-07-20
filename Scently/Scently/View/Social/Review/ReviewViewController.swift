84b37da67abac452d31e64b6fb9602cb409b2144
sy0201 <noey0201@gmail.com>
2025-07-20 16:05:35 +0900

    [Feat] 리뷰쓰기 VC 추가

---
+++ Scently/Scently/View/Social/Review/ReviewViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ReviewViewController.swift
+//  Scently
+//
+//  Created by sy0201 on 7/20/25.
+//
+
+import UIKit
+
+final class ReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+    }
+}
