bda5a74b5b14fcd87ce3dd0cf7c034c993831509
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-07-20 20:47:42 +0900

    [feat] PerfumeResponse 모델 수정, AccordResponse 모델 정의

--- Scently/Scently/Model/PerfumeListResponse.swift
+++ Scently/Scently/Model/PerfumeListResponse.swift
@@ -8,32 +8,15 @@
 import Foundation
 // MARK: - 향수 목록 조회 Responese Model
 struct PerfumeResponse: Codable {
-    let success: Bool
-    let data: PerfumeData?
-    let error: APIError?
-}
-
-struct PerfumeData: Codable {
-    let datalist: [Perfume]
-    let pageInfo: PageInfo
+    let result: String
+    let data: [Perfume]
+    let error: String?
+    let message: String
 }
 
 struct Perfume: Codable {
-    let perfumeId: Int64
+    let perfumeId: Int
     let name: String
     let imageURL: String
     let brand: String
-}
-
-struct PageInfo: Codable {
-    let page: Int
-    let size: Int
-    let totalElements: Int
-    let totalPages: Int
-}
-
-// MARK: - 에러 모델
-struct APIError: Codable {
-    let code: String
-    let message: String
 }
