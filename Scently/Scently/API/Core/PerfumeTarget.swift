c9ab90b8c72222bf10611bbb161cc019d2dcfc89
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-07-20 20:32:16 +0900

    [feat] PerfumeTarget 타입 정의, getPerfumList 포함

--- Scently/Scently/API/Core/PerfumeTarget.swift
+++ Scently/Scently/API/Core/PerfumeTarget.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import Moya
+
+enum PerfumeTarget {
+    case getPerfumList
+}
+
+
+extension PerfumeTarget: TargetType {
+    var baseURL: URL {
+        guard let url = URL(string: AppConfig.shared.baseURL) else {
+            fatalError("Invalid base URL")
+        }
+        return url
+    }
+    
+    var path: String {
+        switch self {
+        case .getPerfumList:
+            return "/api/v1/perfumes"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getPerfumList:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getPerfumList:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        var headers = [
+            "Content=Type:" : "application/json"
+        ]
+        
+        return headers
+    }
+    
+    
+}
