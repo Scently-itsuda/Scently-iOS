827e6c1f932ee8cc2cac37da7dea9296cc762265
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-08-16 21:46:22 +0900

    [feat] myPageProfileView UI작업

---
+++ Scently/Scently/View/MyPage/MyPageProfileView.swift
@@ -0,0 +1,113 @@
+//
+//  MyPageProfileView.swift
+//  Scently
+//
+//  Created by 임재현 on 8/16/25.
+//
+
+import UIKit
+import SnapKit
+
+final class MyPageProfileView: UIView {
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .lightGray
+//        imageView.image = UIImage(named: "LOGO")
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 32
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let usernameLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.font = .pretendard(.medium, size: 16)
+        titleLabel.textColor = .black
+        titleLabel.text = "김아무개"
+        
+        return titleLabel
+    }()
+    
+    let mailLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.font = .pretendard(.medium, size: 12)
+        titleLabel.textColor = .gray3
+        titleLabel.text = "aaaaaa@gmail@com"
+        
+        return titleLabel
+    }()
+    
+    let profileEditButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("프로필 수정", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .pretendard(.medium, size: 11)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.cornerRadius = 6
+        button.backgroundColor = .white
+        
+        return button
+    }()
+    
+    private let dividerView = DividerView()
+
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MyPageProfileView {
+    
+    private func setupUI() {
+        self.addSubviews(
+            profileImageView,
+            usernameLabel,
+            mailLabel,
+            profileEditButton,
+            dividerView
+        )
+   
+    }
+    
+    private func setupConstraints() {
+        profileImageView.snp.makeConstraints {
+            $0.size.equalTo(64)
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        usernameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(8)
+            $0.leading.equalTo(profileImageView.snp.trailing).offset(8)
+        }
+        
+        mailLabel.snp.makeConstraints {
+            $0.top.equalTo(usernameLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(usernameLabel.snp.leading)
+        }
+        
+        profileEditButton.snp.makeConstraints {
+            $0.centerY.equalTo(profileImageView)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.width.equalTo(69)
+            $0.height.equalTo(23)
+        }
+        
+        dividerView.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.bottom).offset(12)  
+            $0.leading.trailing.equalToSuperview()
+        }
+    }
+}
+
+
--- Scently/Scently/View/MyViewController.swift
+++ Scently/Scently/View/MyViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class MyViewController: UIViewController {
         
     private var myPageHeaderView = MyPageHeaderView()
+    private var myPageProfileView = MyPageProfileView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +25,7 @@ final class MyViewController: UIViewController {
 
 extension MyViewController {
     private func setupUI() {
-        self.view.addSubviews(myPageHeaderView)
+        self.view.addSubviews(myPageHeaderView,myPageProfileView)
     }
     private func setupConstraints() {
         myPageHeaderView.snp.makeConstraints {
@@ -33,6 +34,11 @@ extension MyViewController {
             $0.height.equalTo(60)
         }
         
+        myPageProfileView.snp.makeConstraints {
+            $0.top.equalTo(myPageHeaderView.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(64)
+        }
     }
 }
 
@@ -40,5 +46,4 @@ extension MyViewController: MyPageActionDelegate {
     func alertButtonDidTap() {
         print("Alert Button Did Tapped")
     }
-    
 }
