d8386329c513e120176305b590a02f595a1547d6
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-08-31 09:54:20 +0900

    [feat] OOTDDetailResponse 모델링

---
+++ Scently/Scently/Model/OOTDDetailsResponse.swift
@@ -0,0 +1,49 @@
+//
+//  OOTDDetailsResponse.swift
+//  Scently
+//
+//  Created by 임재현 on 8/31/25.
+//
+
+import Foundation
+
+struct OOTDDetailResponse: Codable {
+    let success: Bool
+    let data: OOTDDetailData?
+    let error: String?
+    let message: String
+}
+
+// OOTD 상세 데이터
+struct OOTDDetailData: Codable {
+    let ootdInfo: OOTDInfo
+    let userInfo: UserInfo
+    let perfumeInfo: [PerfumeInfo]
+}
+
+// OOTD 기본 정보
+struct OOTDInfo: Codable {
+    let ootdId: Int
+    let createdAt: Date
+    let ootdImageUrls: [String]
+    let likeCount: Int
+    let commentCount: Int
+    let volume: Int
+    let content: String
+    let tags: [String]
+    let isLiked: Bool
+}
+
+// 사용자 정보
+struct UserInfo: Codable {
+    let gender: String
+    let age: Int
+}
+
+// 향수 정보
+struct PerfumeInfo: Codable {
+    let perfumeId: Int
+    let perfumeBrand: String
+    let perfumeImageUrl: String
+    let perfumeName: String
+}
