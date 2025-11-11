08cb72b778d175496dcb3a66b96c4c7487d12354
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-11-11 17:09:08 +0900

    [feat] #30 LoginCoordinator 객체 구현

---
+++ Scently/Scently/Coordinator/LoginCoordinator.swift
@@ -0,0 +1,39 @@
+//
+//  LoginCoordinator.swift
+//  Scently
+//
+//  Created by 임재현 on 11/11/25.
+//
+
+import UIKit
+
+class LoginCoordinator: Coordinator {
+    
+    var childCoordinators: [Coordinator] = []
+    
+    private let navigationConotroller: UINavigationController
+    private let authService: AuthServiceProtocol
+    
+    var onLoginSuccess: (()->Void)?
+    var onGuestMode: (()->Void)?
+    
+    init(navigationConotroller: UINavigationController, authService: AuthServiceProtocol) {
+        self.navigationConotroller = navigationConotroller
+        self.authService = authService
+    }
+    
+    func start() {
+        let loginVC = LoginViewController()
+       // loginVC.coordinator = self
+        navigationConotroller.setViewControllers([loginVC], animated: false)
+    }
+    
+    func didTapLogin(email: String, password: String) {
+        authService.saveToken("dummy_token_\(email)")
+        onLoginSuccess?()
+    }
+    
+    func didTapGuestMode() {
+        onGuestMode?()
+    }
+}
