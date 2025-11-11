ef129444f6793c1b75eb43e2e4181847cfd214ff
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-11-11 16:49:46 +0900

    [feat] AppCoordinator 에 authService 의존성 주입

--- Scently/Scently/Coordinator/AppCoordinator.swift
+++ Scently/Scently/Coordinator/AppCoordinator.swift
@@ -18,12 +18,20 @@ class AppCoordinator: Coordinator {
     var childCoordinators: [Coordinator] = []
     
     private let window: UIWindow
+    private let authService: AuthServiceProtocol
     
-    init(window: UIWindow) {
+    init(window: UIWindow, authservice: AuthServiceProtocol) {
         self.window = window
+        self.authService = authservice
     }
     
     func start() {
         print("Coordinator 시작")
+        
+        if authService.hasValidToken() {
+            print("토큰 있음 - MainTab으로 이동 예정")
+        } else {
+            print("토큰 없음 - Login으로 이동 예정")
+        }
     }
 }
