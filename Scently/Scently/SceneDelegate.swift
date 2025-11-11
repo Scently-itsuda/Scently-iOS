c00ba7efcd9639f542f673538029981dc472c899
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-11-11 16:33:06 +0900

    [feat] #30 SceneDelegate 에 Appcoordinator 연결 작업

--- Scently/Scently/SceneDelegate.swift
+++ Scently/Scently/SceneDelegate.swift
@@ -10,16 +10,21 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
    
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let tabViewController = LoginViewController()
-        window.rootViewController = tabViewController
-        window.makeKeyAndVisible()
         self.window = window
+        
+        appCoordinator = AppCoordinator(window: window)
+        appCoordinator?.start()
+        window.makeKeyAndVisible()
+        
+//        let tabViewController = LoginViewController()
+//        window.rootViewController = tabViewController
         
     }
 }
