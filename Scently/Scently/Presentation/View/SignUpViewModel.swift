e35ce03edfa9c7ef645a9cd3d98a4636cb8649e8
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-11-13 10:53:31 +0900

    [feat]#32 1차 유효성 검사 로직 구현

--- Scently/Scently/Presentation/View/SignUpViewModel.swift
+++ Scently/Scently/Presentation/View/SignUpViewModel.swift
@@ -9,18 +9,25 @@ import UIKit
 import Combine
 
 final class SignupViewModel {
+    
+    private let authService: AuthServiceProtocol
+    
     @Published var selectedGender: Gender?
     @Published var birthDate: Date?
     @Published var isBirthDatePrivate: Bool = false
     @Published var nickname: String = ""
-    @Published var isNicknameVerified: Bool = false
+    
+    @Published var nicknameValidationState: NicknameValidationState = .empty
+    
     
     @Published var isFormValid: Bool = false
+    @Published var canCheckDuplicate: Bool = false
     @Published var shouldHilightedDuplicatedCheck: Bool = false
     
     private var cancellable = Set<AnyCancellable>()
     
-    init() {
+    init(authService: AuthServiceProtocol) {
+        self.authService = authService
         setupBindings()
     }
 }
@@ -28,44 +35,71 @@ final class SignupViewModel {
 extension SignupViewModel {
     
     private func setupBindings() {
+        
+        $nickname
+            .debounce(for: 0.3, scheduler: DispatchQueue.main)
+            .map { [weak self] text in
+                self?.validateNicknameFormat(text) ?? .empty
+            }
+            .sink { [weak self] state in
+                self?.nicknameValidationState = state
+            }
+            .store(in: &cancellable)
+        
+        $nicknameValidationState
+            .map { state in
+                if case .validButNotChecked = state {
+                    return true
+                }
+                
+                return false
+            }
+            .assign(to: &$canCheckDuplicate)
+        
+        $nicknameValidationState
+            .map { state in
+                if case .validButNotChecked = state {
+                    return true
+                }
+                return false
+            }
+            .assign(to: &$shouldHilightedDuplicatedCheck)
+        
         Publishers.CombineLatest4(
             $selectedGender,
             $birthDate,
             $isBirthDatePrivate,
-            $isNicknameVerified
+            $nicknameValidationState
         )
-        .map { gender, date, isPrivate, nicknameVerified  in
+        .map { gender, date, isPrivate, nicknameState  in
             let genderValid = gender != nil // 성별선택완료
             let birthDateValid = date != nil || isPrivate // 생년월일 입력 or 비공개
-            let nicknameValid = nicknameVerified // 닉네임 중복확인
+            
+            let nicknameValid: Bool
+            
+            if case .available = nicknameState {
+                nicknameValid = true
+            } else {
+                nicknameValid = false
+            }
             
             return genderValid && birthDateValid && nicknameValid
         }
         .assign(to: &$isFormValid)
         
-        Publishers.CombineLatest($nickname , $isNicknameVerified)
-            .map { nickname, verified in
-                return nickname.count >= 2 && !verified
-            }
-            .assign(to: &$shouldHilightedDuplicatedCheck)
-        
-    }
-    
-    func resetNicknameVerification() {
-        isNicknameVerified = false
-    }
-    
-    func completeNicknameVerification() {
-        isNicknameVerified = false
     }
     
     func canSubmit() -> (canSubmit: Bool, errorMessage: String?) {
         if nickname.isEmpty {
             return (false, "닉네임을 입력해주세요")
         }
-        if !isNicknameVerified {
+        
+        if case .available = nicknameValidationState {
+           
+        } else {
             return (false, "중복확인을 해주세요")
         }
+        
         
         if selectedGender == nil {
             return (false, "성별을 선택해주세요")
@@ -77,5 +111,31 @@ extension SignupViewModel {
         
         return (true, nil)
     }
-
+    
+    private func validateNicknameFormat(_ text: String) -> NicknameValidationState {
+        if text.isEmpty {
+            return .empty
+        }
+        
+        if text.count < 2 {
+            return .invalid(reason: "닉네임은 2글자 이상이여야 합니다")
+        }
+        
+        if text.count < 10 {
+            return .invalid(reason: "닉네임은 10글자 이하로 입력해주세요")
+        }
+        
+        let allowedCharacters = CharacterSet.alphanumerics
+            .union(CharacterSet(charactersIn: "가-힣"))
+        
+        if text.rangeOfCharacter(from: allowedCharacters.inverted) != nil {
+            return .invalid(reason: "한글, 영문, 숫자만 입력 가능해요")
+        }
+        
+        return .validButNotChecked
+    }
+    
+    func checkNicknameDuplicate() {
+        
+    }
 }
