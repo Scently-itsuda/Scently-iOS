bef49e99116b6936f6d36d6981aa0d2144b31396
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-11-14 12:56:03 +0900

    [feat] #34 OOTD/FreeBoard/Review WriteVC 생성

---
+++ Scently/Scently/Presentation/View/Social/FreeBoardWriteViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FreeBoardWriteViewController.swift
+//  Scently
+//
+//  Created by 임재현 on 11/14/25.
+//
+
+import UIKit
+
+final class FreeBoardWriteViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+}
+
+extension FreeBoardWriteViewController {
+    private func setupUI() {
+        self.view.backgroundColor = .blue
+    }
+}
---
+++ Scently/Scently/Presentation/View/Social/OOTDWriteViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OOTDWriteViewController.swift
+//  Scently
+//
+//  Created by 임재현 on 11/14/25.
+//
+
+import UIKit
+
+final class OOTDWriteViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+}
+
+extension OOTDWriteViewController {
+    private func setupUI() {
+        self.view.backgroundColor = .red
+    }
+}
